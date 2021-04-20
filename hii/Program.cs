36cad35d9fb4f29cd1b90602ab691fab38019327
JayalakshmiBhupathiraju <82873576+JayalakshmiBhupathiraju@users.noreply.github.com>
2021-04-20 22:18:08 +0530

    first commit

---
+++ hii/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace hii
+{
+    class Program
+    {
+//        static void Main(string []args)
+//        {
+//            Console.WriteLine("Enter the name");
+//            string name = Console.ReadLine();
+//            Console.WriteLine("How much sleep did you have last night");
+//            int hoursOfSleep = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Hello World!"+name);
+//            if(hoursOfSleep<=8)
+//            {
+//                Console.WriteLine("you must feel tired");
+//            }
+//            else
+//            {
+//                Console.WriteLine("you seem well rested");
+//            }
+//        }
+    }
+}
