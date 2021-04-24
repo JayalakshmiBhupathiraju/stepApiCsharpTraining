634498e7fb5889ea2df51e4d71d4eef63524c6db
JayalakshmiBhupathiraju <82873576+JayalakshmiBhupathiraju@users.noreply.github.com>
2021-04-24 11:35:35 +0530

    C#

---
+++ C# Fundamentals/PropertiesDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PropertiesDemo
+{
+    public class TimePeriod
+    {
+        private double _seconds;
+        public double Hours
+        {
+            get { return _seconds; }
+            set { _seconds = value * 3600; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter time in hours");
+            TimePeriod t = new TimePeriod();
+            string Input;
+            Input= Console.ReadLine();
+            int hours = Int32.Parse(Input);
+            t.Hours = hours;
+            Console.WriteLine("Time in seconds {0}",t.Hours);
+        }
+    }
+}
