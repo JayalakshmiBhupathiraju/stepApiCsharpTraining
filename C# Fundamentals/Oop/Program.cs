2502aaf721bf087f409c53f2e39050ba75b70ac0
JayalakshmiBhupathiraju <82873576+JayalakshmiBhupathiraju@users.noreply.github.com>
2021-04-24 17:12:24 +0530

    C#

---
+++ C# Fundamentals/Oop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Oop
+{
+     class Animal
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal makes sound");
+        }
+    }
+    
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says:wee wee");
+        }
+    }
+    abstract class Eating
+    {
+        public abstract void sleep();
+        public void eating()
+        {
+            Console.WriteLine("Eating....");
+        }
+
+    }
+    class Dog:Eating
+    {
+        public override void sleep()
+        {
+            Console.WriteLine("Zzz..");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal obj =new Animal();
+            Animal obj1 = new Pig();
+            Dog obj2 = new Dog();
+            obj.animalSound();
+            obj1.animalSound();
+            obj2.sleep();
+            obj2.eating();
+        }
+    }
+}
